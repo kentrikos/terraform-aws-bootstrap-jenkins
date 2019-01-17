6d2df3b2323398d9c8a96cd96da9452d256bf9d8
Radosław Palczyński <radoslaw.palczynski@nordcloud.com>
2019-01-17 11:25:00 +0100

    add missing provider file

---
+++ provider.tf
@@ -0,0 +1,30 @@
+# AWS region must be set externally, e.g. by exporting AWS_DEFAULT_REGION env variable
+
+provider "aws" {
+  region  = "${var.region}"
+  version = " ~> v1.55"
+}
+
+provider "http" {
+  version = " ~> v1.0.1"
+}
+
+provider "local" {
+  version = " ~> v1.1.0"
+}
+
+provider "null" {
+  version = " ~> v1.0.0"
+}
+
+provider "random" {
+  version = " ~> v2.0.0"
+}
+
+provider "template" {
+  version = " ~> v1.0.0"
+}
+
+provider "tls" {
+  version = " ~> v1.2.0"
+}
