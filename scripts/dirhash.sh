b3bf0f116ff989efad945c985c2965abbdde2151
Radosław Palczyński <radoslaw.palczynski@nordcloud.com>
2019-05-17 13:02:28 +0200

    fix dirsha.sh script

--- scripts/dirhash.sh
+++ scripts/dirhash.sh
@@ -13,7 +13,7 @@ set -e
 eval "$(jq -r '@sh "DIRECTORY=\(.directory)"')"
 
 # Placeholder for whatever data-fetching logic your script implements
-CHECKSUM=`find ${DIRECTORY} -type f | LC_ALL=C sort | xargs shasum -a 256 | awk '{ n=split ($2, tokens, /\//); print $1 " " tokens[n]} ' |  shasum -a 256 | awk '{ print $1 }'`
+CHECKSUM=`find ${DIRECTORY} -type f | LC_ALL=C sort | xargs sha256sum   | awk '{ n=split ($2, tokens, /\//); print $1 " " tokens[n]} ' |  sha256sum  | awk '{ print $1 }'`
 
 # Safely produce a JSON object containing the result value.
 # jq will ensure that the value is properly quoted
